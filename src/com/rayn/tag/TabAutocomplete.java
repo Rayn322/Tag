e3e6778fcbf0a24d71a2fcb67d61fb5a714e8125
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2020-08-24 17:34:06 -0700

    Added autocomplete for the command.

---
+++ src/com/rayn/tag/TabAutocomplete.java
@@ -0,0 +1,28 @@
+package com.rayn.tag;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TabAutocomplete implements TabCompleter {
+    
+    @Override
+    public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] args) {
+        
+        if (args.length == 1) {
+            
+            List<String> startOrStop = new ArrayList<>();
+            startOrStop.add("start");
+            startOrStop.add("stop");
+            
+            return startOrStop;
+            
+            }
+         
+        return null;
+        
+        }
+}
