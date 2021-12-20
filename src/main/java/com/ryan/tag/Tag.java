5ade06a2787ec02efe226e223e970a66cceb2d62
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2021-12-20 14:41:22 -0800

    Add update checker and check if the server is a Paper server

--- src/main/java/com/ryan/tag/Tag.java
+++ src/main/java/com/ryan/tag/Tag.java
@@ -8,12 +8,17 @@ import com.ryan.tag.gameplay.Game;
 import com.ryan.tag.gameplay.TagInfoDisplay;
 import com.ryan.tag.listener.PreventDamage;
 import com.ryan.tag.listener.TagPlayer;
+import com.ryan.tag.util.CurseAPI;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.IOException;
+
 public final class Tag extends JavaPlugin {
     
+    public static String version = "1.0.2";
+    private static boolean isPaper = true;
     private static Tag plugin;
     
     public static Tag getPlugin() {
@@ -22,6 +27,15 @@ public final class Tag extends JavaPlugin {
     
     @Override
     public void onEnable() {
+        try {
+            Class.forName("io.papermc.paper.event.player.AsyncChatEvent");
+        } catch (ClassNotFoundException e) {
+            getLogger().severe("Paper is not installed! Disabling plugin.");
+            getLogger().severe("Please download paper at https://papermc.io/downloads");
+            isPaper = false;
+            return;
+        }
+    
         plugin = this;
         new Metrics(this, 8787);
         registerEvents();
@@ -31,10 +45,20 @@ public final class Tag extends JavaPlugin {
         saveConfig();
         TagSettings.readFromConfig();
         TagInfoDisplay.createTeams();
+    
+        try {
+            CurseAPI.checkForUpdates();
+        } catch (IOException e) {
+            getLogger().warning("Failed to check for updates!");
+        }
     }
     
     @Override
     public void onDisable() {
+        if (!isPaper) {
+            return;
+        }
+        
         if (Game.isPlaying) {
             Game.stop();
             getLogger().info(ChatColor.RED + "Stopping game because the server has stopped.");
