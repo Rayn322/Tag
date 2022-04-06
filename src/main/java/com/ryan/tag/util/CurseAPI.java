06e64bce89577c7a42cc0029fef46453211fad26
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2022-04-06 09:48:30 -0700

    Add extra log when starting to check for updates

--- src/main/java/com/ryan/tag/util/CurseAPI.java
+++ src/main/java/com/ryan/tag/util/CurseAPI.java
@@ -15,6 +15,7 @@ public class CurseAPI {
     // https://github.com/google/gson/blob/master/UserGuide.md#collections-examples
     @SuppressWarnings("UnstableApiUsage")
     public static void checkForUpdates() throws IOException {
+        Tag.getPlugin().getLogger().info("Checking for updates...");
         URL url = new URL("https://api.curseforge.com/servermods/files?projectIds=405163");
         InputStreamReader reader = new InputStreamReader(url.openStream());
         Type collectionType = new TypeToken<List<File>>(){}.getType();
